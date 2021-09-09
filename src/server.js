ba2c5946d11c828dc7f0b2fe8760bfa4f9ec535b
Caio Seiji Pracideli <cpracideli@gmail.com>
2021-09-09 03:37:01 -0300

    Update server.js
    change app listen port to log on console

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = require('./routes')
+const PORT = process.env.PORT || 5000
 
 require('./database');
 
@@ -8,4 +9,4 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(80);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
